79f9b6c3560f498e701e1b453bf50be72ee89c76
kanhaiya04 <kanhaiyayadav9999@gmail.com>
2023-10-08 12:01:04 +0530

    added logger to the server

---
+++ server/logger.js
@@ -0,0 +1,32 @@
+const winston = require('winston');
+
+// Create logger instance
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  defaultMeta: { service: 'docuSync-server' },
+  transports: [
+    // Write all logs with importance level of `error` or less to `error.log`
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    // Write all logs with importance level of `info` or less to `combined.log`
+    new winston.transports.File({ filename: 'logs/combined.log' })
+  ]
+});
+
+// If we're not in production then log to the console with a simple format
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    )
+  }));
+}
+
+module.exports = logger;
