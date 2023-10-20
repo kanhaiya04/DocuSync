be146f6edff8969fec993ba42f27cf746a6cb243
kanhaiya04 <kanhaiyayadav9999@gmail.com>
2023-10-20 21:19:59 +0530

    fix: login and signup

--- client/app/src/scences/Login.tsx
+++ client/app/src/scences/Login.tsx
@@ -6,47 +6,122 @@ import {
   Form,
   Row,
   Stack,
+  Alert,
 } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+interface ValidationErrors {
+  email: string;
+  password: string;
+}
+
 const Login: React.FC = () => {
   let navigator = useNavigate();
   const [cred, setCred] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState<ValidationErrors>({ email: "", password: "" });
+  const [touched, setTouched] = useState({ email: false, password: false });
   const ipcRendener = (window as any).ipcRenderer;
   const configStore = (window as any).configStore;
-  const host = "http://localhost:5000";
+  const host = "https://docusync-server.onrender.com";
   useEffect(() => {
     if (configStore.getSecret()) navigator("/");
   });
 
-  const handleClick = async () => {
-    const response = await fetch(`${host}/user/login`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ email: cred.email, password: cred.password }),
+  // Validation functions
+  const validateEmail = (email: string): string => {
+    if (!email.trim()) {
+      return "Email is required";
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return "Please enter a valid email address";
+    }
+    return "";
+  };
+
+  const validatePassword = (password: string): string => {
+    if (!password.trim()) {
+      return "Password is required";
+    }
+    if (password.length < 6) {
+      return "Password must be at least 6 characters long";
+    }
+    return "";
+  };
+
+  const validateForm = (): boolean => {
+    const emailError = validateEmail(cred.email);
+    const passwordError = validatePassword(cred.password);
+    
+    setErrors({
+      email: emailError,
+      password: passwordError
     });
-    const json = await response.json();
-    if (json.success) {
-      const token = json.token;
-      ipcRendener.send("key:set", {
-        token: token,
+    
+    setTouched({ email: true, password: true });
+    
+    return !emailError && !passwordError;
+  };
+
+  const handleClick = async () => {
+    if (!validateForm()) {
+      toast.error("Please fix the errors before submitting");
+      return;
+    }
+    try {
+      const response = await fetch(`${host}/user/login`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ email: cred.email, password: cred.password }),
       });
-      navigator("/");
-    } else {
-      ipcRendener.send("error",{
-        "error":json.err,
-        "msg":json.msg?json.msg:"" 
-      })
+      const json = await response.json();
+      if (json.success) {
+        const token = json.token;
+        ipcRendener.send("key:set", {
+          token: token,
+        });
+        toast.success("Login successful! Welcome back!");
+        navigator("/");
+      } else {
+        toast.error(json.msg || json.err || "Login failed. Please try again.");
+      }
+    } catch (error) {
+      toast.error("Network error. Please check your connection and try again.");
     }
   };
 
   const handleChange = (e: any) => {
-    if (e.target.name === "email") {
-      setCred({ email: e.target.value, password: cred.password });
-    } else {
-      setCred({ email: cred.email, password: e.target.value });
+    const { name, value } = e.target;
+    setCred(prev => ({ ...prev, [name]: value }));
+    
+    // Real-time validation
+    if (touched[name as keyof typeof touched]) {
+      let error = "";
+      if (name === "email") {
+        error = validateEmail(value);
+      } else if (name === "password") {
+        error = validatePassword(value);
+      }
+      
+      setErrors(prev => ({ ...prev, [name]: error }));
     }
+  };
+
+  const handleBlur = (e: any) => {
+    const { name, value } = e.target;
+    setTouched(prev => ({ ...prev, [name]: true }));
+    
+    let error = "";
+    if (name === "email") {
+      error = validateEmail(value);
+    } else if (name === "password") {
+      error = validatePassword(value);
+    }
+    
+    setErrors(prev => ({ ...prev, [name]: error }));
   };
   return (
     <Container
@@ -84,43 +159,83 @@ const Login: React.FC = () => {
           <Col sm={6} className="d-flex align-items-center">
             <div style={{ width: "100%" }}>
               <Stack gap={3} style={{ alignItems: "center" }}>
-                <FloatingLabel
-                  controlId="floatingInput"
-                  label="Email address"
-                  style={{
-                    color: "white",
-                    width: "70%",
-                    backgroundColor: "#213555",
-                  }}
-                >
-                  <Form.Control
-                    type="email"
-                    name="email"
-                    required
-                    placeholder="name@example.com"
-                    onChange={handleChange}
-                    style={{ backgroundColor: "#213555", color: "white" }}
-                  />
-                </FloatingLabel>
+                <div style={{ width: "70%" }}>
+                  <FloatingLabel
+                    controlId="floatingInput"
+                    label="Email address"
+                    style={{
+                      color: "white",
+                      backgroundColor: "#213555",
+                    }}
+                  >
+                    <Form.Control
+                      type="email"
+                      name="email"
+                      required
+                      placeholder="name@example.com"
+                      value={cred.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      isInvalid={touched.email && !!errors.email}
+                      style={{ 
+                        backgroundColor: "#213555", 
+                        color: "white",
+                        borderColor: touched.email && errors.email ? "#dc3545" : "#213555"
+                      }}
+                    />
+                  </FloatingLabel>
+                  {touched.email && errors.email && (
+                    <Alert variant="danger" style={{ 
+                      marginTop: "5px", 
+                      padding: "5px 10px", 
+                      fontSize: "0.875rem",
+                      backgroundColor: "rgba(220, 53, 69, 0.1)",
+                      border: "1px solid #dc3545",
+                      color: "#dc3545"
+                    }}>
+                      {errors.email}
+                    </Alert>
+                  )}
+                </div>
 
-                <FloatingLabel
-                  controlId="floatingInput"
-                  label="Password"
-                  style={{
-                    color: "white",
-                    width: "70%",
-                    backgroundColor: "#213555",
-                  }}
-                >
-                  <Form.Control
-                    type="password"
-                    name="password"
-                    required
-                    placeholder="name@example.com"
-                    onChange={handleChange}
-                    style={{ backgroundColor: "#213555", color: "white" }}
-                  />
-                </FloatingLabel>
+                <div style={{ width: "70%" }}>
+                  <FloatingLabel
+                    controlId="floatingPassword"
+                    label="Password"
+                    style={{
+                      color: "white",
+                      backgroundColor: "#213555",
+                    }}
+                  >
+                    <Form.Control
+                      type="password"
+                      name="password"
+                      required
+                      placeholder="Enter your password"
+                      value={cred.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      isInvalid={touched.password && !!errors.password}
+                      style={{ 
+                        backgroundColor: "#213555", 
+                        color: "white",
+                        borderColor: touched.password && errors.password ? "#dc3545" : "#213555"
+                      }}
+                    />
+                  </FloatingLabel>
+                  {touched.password && errors.password && (
+                    <Alert variant="danger" style={{ 
+                      marginTop: "5px", 
+                      padding: "5px 10px", 
+                      fontSize: "0.875rem",
+                      backgroundColor: "rgba(220, 53, 69, 0.1)",
+                      border: "1px solid #dc3545",
+                      color: "#dc3545"
+                    }}>
+                      {errors.password}
+                    </Alert>
+                  )}
+                </div>
 
                 <button
                   type="button"
--- client/app/src/scences/SignUp.tsx
+++ client/app/src/scences/SignUp.tsx
@@ -6,8 +6,16 @@ import {
   Form,
   Row,
   Stack,
+  Alert,
 } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+interface ValidationErrors {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
   let navigator = useNavigate();
@@ -17,41 +25,127 @@ const SignUp: React.FC = () => {
   const ipcRendener = (window as any).ipcRenderer;
   const configStore = (window as any).configStore;
   const [cred, setCred] = useState({ name: "", email: "", password: "" });
-  const host = "http://localhost:5000";
+  const [errors, setErrors] = useState<ValidationErrors>({ name: "", email: "", password: "" });
+  const [touched, setTouched] = useState({ name: false, email: false, password: false });
+  const host = "https://docusync-server.onrender.com";
   
+  // Validation functions
+  const validateName = (name: string): string => {
+    if (!name.trim()) {
+      return "Name is required";
+    }
+    if (name.trim().length < 2) {
+      return "Name must be at least 2 characters long";
+    }
+    if (!/^[a-zA-Z\s]+$/.test(name.trim())) {
+      return "Name can only contain letters and spaces";
+    }
+    return "";
+  };
+
+  const validateEmail = (email: string): string => {
+    if (!email.trim()) {
+      return "Email is required";
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return "Please enter a valid email address";
+    }
+    return "";
+  };
+
+  const validatePassword = (password: string): string => {
+    if (!password.trim()) {
+      return "Password is required";
+    }
+    if (password.length < 6) {
+      return "Password must be at least 6 characters long";
+    }
+    return "";
+  };
+
+  const validateForm = (): boolean => {
+    const nameError = validateName(cred.name);
+    const emailError = validateEmail(cred.email);
+    const passwordError = validatePassword(cred.password);
+    
+    setErrors({
+      name: nameError,
+      email: emailError,
+      password: passwordError
+    });
+    
+    setTouched({ name: true, email: true, password: true });
+    
+    return !nameError && !emailError && !passwordError;
+  };
 
   const handleClick = async () => {
-    const response = await fetch(`${host}/user/createuser`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        name: cred.name,
-        email: cred.email,
-        password: cred.password,
-      }),
-    });
-    const json = await response.json();
-    if (json.success) {
-      const token = json.token;
-      ipcRendener.send("key:set", {
-        token: token,
+    if (!validateForm()) {
+      toast.error("Please fix the errors before submitting");
+      return;
+    }
+    try {
+      const response = await fetch(`${host}/user/createuser`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          name: cred.name,
+          email: cred.email,
+          password: cred.password,
+        }),
       });
-      navigator("/");
-    } else {
-      ipcRendener.send("error",{
-        "error":json.err,
-        "msg":json.msg?json.msg:"" 
-      })
+      const json = await response.json();
+      if (json.success) {
+        const token = json.token;
+        ipcRendener.send("key:set", {
+          token: token,
+        });
+        toast.success("Account created successfully! Welcome to DocuSync!");
+        navigator("/");
+      } else {
+        toast.error(json.msg || json.err || "Sign up failed. Please try again.");
+      }
+    } catch (error) {
+      toast.error("Network error. Please check your connection and try again.");
     }
   };
 
   const handleChange = (e: any) => {
-    setCred((prev) => ({
-      ...prev,
-      [e.target.name]: e.target.value,
-    }));
+    const { name, value } = e.target;
+    setCred(prev => ({ ...prev, [name]: value }));
+    
+    // Real-time validation
+    if (touched[name as keyof typeof touched]) {
+      let error = "";
+      if (name === "name") {
+        error = validateName(value);
+      } else if (name === "email") {
+        error = validateEmail(value);
+      } else if (name === "password") {
+        error = validatePassword(value);
+      }
+      
+      setErrors(prev => ({ ...prev, [name]: error }));
+    }
+  };
+
+  const handleBlur = (e: any) => {
+    const { name, value } = e.target;
+    setTouched(prev => ({ ...prev, [name]: true }));
+    
+    let error = "";
+    if (name === "name") {
+      error = validateName(value);
+    } else if (name === "email") {
+      error = validateEmail(value);
+    } else if (name === "password") {
+      error = validatePassword(value);
+    }
+    
+    setErrors(prev => ({ ...prev, [name]: error }));
   };
   return (
     <Container
@@ -89,61 +183,121 @@ const SignUp: React.FC = () => {
           <Col sm={6} className="d-flex align-items-center">
             <div style={{ width: "100%" }}>
               <Stack gap={3} style={{ alignItems: "center" }}>
-                <FloatingLabel
-                  controlId="floatingInput"
-                  label="Name"
-                  style={{
-                    color: "white",
-                    width: "70%",
-                    backgroundColor: "#213555",
-                  }}
-                >
-                  <Form.Control
-                    type="text"
-                    name="name"
-                    required
-                    placeholder="Name"
-                    onChange={handleChange}
-                    style={{ backgroundColor: "#213555", color: "white" }}
-                  />
-                </FloatingLabel>
-                <FloatingLabel
-                  controlId="floatingInput"
-                  label="Email address"
-                  style={{
-                    color: "white",
-                    width: "70%",
-                    backgroundColor: "#213555",
-                  }}
-                >
-                  <Form.Control
-                    type="email"
-                    name="email"
-                    required
-                    placeholder="name@example.com"
-                    onChange={handleChange}
-                    style={{ backgroundColor: "#213555", color: "white" }}
-                  />
-                </FloatingLabel>
+                <div style={{ width: "70%" }}>
+                  <FloatingLabel
+                    controlId="floatingName"
+                    label="Name"
+                    style={{
+                      color: "white",
+                      backgroundColor: "#213555",
+                    }}
+                  >
+                    <Form.Control
+                      type="text"
+                      name="name"
+                      required
+                      placeholder="Enter your full name"
+                      value={cred.name}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      isInvalid={touched.name && !!errors.name}
+                      style={{ 
+                        backgroundColor: "#213555", 
+                        color: "white",
+                        borderColor: touched.name && errors.name ? "#dc3545" : "#213555"
+                      }}
+                    />
+                  </FloatingLabel>
+                  {touched.name && errors.name && (
+                    <Alert variant="danger" style={{ 
+                      marginTop: "5px", 
+                      padding: "5px 10px", 
+                      fontSize: "0.875rem",
+                      backgroundColor: "rgba(220, 53, 69, 0.1)",
+                      border: "1px solid #dc3545",
+                      color: "#dc3545"
+                    }}>
+                      {errors.name}
+                    </Alert>
+                  )}
+                </div>
+                <div style={{ width: "70%" }}>
+                  <FloatingLabel
+                    controlId="floatingEmail"
+                    label="Email address"
+                    style={{
+                      color: "white",
+                      backgroundColor: "#213555",
+                    }}
+                  >
+                    <Form.Control
+                      type="email"
+                      name="email"
+                      required
+                      placeholder="name@example.com"
+                      value={cred.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      isInvalid={touched.email && !!errors.email}
+                      style={{ 
+                        backgroundColor: "#213555", 
+                        color: "white",
+                        borderColor: touched.email && errors.email ? "#dc3545" : "#213555"
+                      }}
+                    />
+                  </FloatingLabel>
+                  {touched.email && errors.email && (
+                    <Alert variant="danger" style={{ 
+                      marginTop: "5px", 
+                      padding: "5px 10px", 
+                      fontSize: "0.875rem",
+                      backgroundColor: "rgba(220, 53, 69, 0.1)",
+                      border: "1px solid #dc3545",
+                      color: "#dc3545"
+                    }}>
+                      {errors.email}
+                    </Alert>
+                  )}
+                </div>
 
-                <FloatingLabel
-                  controlId="floatingInput"
-                  label="Password"
-                  style={{
-                    color: "white",
-                    width: "70%",
-                    backgroundColor: "#213555",
-                  }}
-                >
-                  <Form.Control
-                    type="password"
-                    name="password"
-                    required
-                    placeholder="name@example.com"
-                    onChange={handleChange}
-                    style={{ backgroundColor: "#213555", color: "white" }}
-                  />
-                </FloatingLabel>
+                <div style={{ width: "70%" }}>
+                  <FloatingLabel
+                    controlId="floatingPassword"
+                    label="Password"
+                    style={{
+                      color: "white",
+                      backgroundColor: "#213555",
+                    }}
+                  >
+                    <Form.Control
+                      type="password"
+                      name="password"
+                      required
+                      placeholder="Enter a strong password"
+                      value={cred.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      isInvalid={touched.password && !!errors.password}
+                      style={{ 
+                        backgroundColor: "#213555", 
+                        color: "white",
+                        borderColor: touched.password && errors.password ? "#dc3545" : "#213555"
+                      }}
+                    />
+                  </FloatingLabel>
+                  {touched.password && errors.password && (
+                    <Alert variant="danger" style={{ 
+                      marginTop: "5px", 
+                      padding: "5px 10px", 
+                      fontSize: "0.875rem",
+                      backgroundColor: "rgba(220, 53, 69, 0.1)",
+                      border: "1px solid #dc3545",
+                      color: "#dc3545"
+                    }}>
+                      {errors.password}
+                    </Alert>
+                  )}
+                </div>
 
                 <button
                   type="button"
